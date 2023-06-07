36acd27a75340cb89d27ffc9a41942e6702b0ad2
juanalsan <118403734+juanalsan@users.noreply.github.com>
2023-06-07 15:29:52 +0200

    Add files via upload

---
+++ ej1.py
@@ -0,0 +1,86 @@
+import sys
+from pyspark import SparkContext
+
+
+
+
+def mapper(line):
+    edge = line.strip().split(',')
+    n1  = edge[0]
+    n2  = edge[1]
+
+    if n1 < n2:
+        return (n1, n2) 
+    elif n1 > n2:         
+        return (n2, n1)    
+    else:
+        pass 
+
+
+     
+def distintas(spc, filename):
+    """
+Recibe el fichero,calcula los vértices de cada línea y devuelve los elementos no repetidos
+    """
+    return spc.textFile(filename).map(mapper).filter(lambda x: x is not None).distinct() 
+
+
+
+def pista(adyacencia):
+    """
+Función que recomienda el apartado 2 del enunciado
+    """
+    conexiones = []
+    for i in range(len(adyacencia[1])):
+        conexiones.append(((adyacencia[0], adyacencia[1][i]), 'exists')) 
+
+        for j in range(i+1,len(adyacencia[1])):
+            n1 = adyacencia[1][i]
+            n2 = adyacencia[1][j]
+            v1 = min(n1,n2)
+            v2 = min(n1,n2)
+            conexiones.append(((v1,v2),('pending',adyacencia[0])))
+
+    return conexiones
+
+
+  
+def condicion(adyacencia):
+    return (len(adyacencia[1]) >= 2 and 'exists' in adyacencia[1])
+
+
+   
+def posibles_triciclos(adyacencia):
+    """
+Calcula los posibles triciclos
+    """
+    posib = []
+    for elem in adyacencia[1]:
+        if elem != 'exists':
+            posib.append((adyacencia[1], adyacencia[0][0], adyacencia[0][1]))
+
+    return posib
+
+
+    
+def ejercicio1_grafos(spc, filename):
+    """
+calcula los 3-ciclos de un grafo definido como lista de aristas
+
+    """
+    aristas   = distintas(spc, filename)
+    conectados = aristas.groupByKey().mapValues(list).flatMap(pista)
+    triciclos = conectados.groupByKey().mapValues(list).filter(condicion).flatMap(posibles_triciclos)
+
+    print(triciclos.collect())
+    return triciclos.collect()
+    
+
+if __name__ == "__main__":
+    spc = SparkContext()
+    try:
+        filename = sys.argv[1]
+    except:
+        filename = input('Introduce el nombre del fichero : ')
+
+    ejercicio1_grafos(spc, filename)
---
+++ ej3.py
@@ -0,0 +1,91 @@
+import sys
+from pyspark import SparkContext
+
+
+
+def mapper(line):
+    edge = line.strip().split(',')
+    n1  = edge[0]
+    n2  = edge[1]
+
+    if n1 < n2:
+        return (n1, n2) 
+    elif n1 > n2:         
+        return (n2, n1)    
+    else:
+        pass 
+
+
+     
+def distintas(spc, filename):
+    """
+Recibe el fichero,calcula los vértices de cada línea y devuelve los elementos no repetidos
+    """
+    return spc.textFile(filename).map(mapper).filter(lambda x: x is not None).distinct() 
+
+
+
+def pista(adyacencia):
+    """
+Función que recomienda el apartado 2 del enunciado
+    """
+    conexiones = []
+    for i in range(len(adyacencia[1])):
+        conexiones.append(((adyacencia[0], adyacencia[1][i]), 'exists')) 
+
+        for j in range(i+1,len(adyacencia[1])):
+            n1 = adyacencia[1][i]
+            n2 = adyacencia[1][j]
+            v1 = min(n1,n2)
+            v2 = min(n1,n2)
+            conexiones.append(((v1,v2),('pending',adyacencia[0])))
+
+    return conexiones
+
+
+  
+def condicion(adyacencia):
+    return (len(adyacencia[1]) >= 2 and 'exists' in adyacencia[1])
+
+
+   
+def posibles_triciclos(adyacencia):
+    """
+Calcula los posibles triciclos
+    """
+    posib = []
+    for elem in adyacencia[1]:
+        if elem != 'exists':
+            posib.append((adyacencia[1], adyacencia[0][0], adyacencia[0][1]))
+
+    return posib
+
+
+    
+def ejercicio3_grafos(spc, filenames):
+    """
+calcula los 3-ciclos locales a cada uno de los ficheros de la lista de ficheros de entrada. 
+
+    """
+    triciclos_locales_lst=[]
+    for filename in filenames :
+        file_rdd= spc.textfile(filename)
+        aristas = file_rdd.map(distintas).filter(lambda x: x is not None).distinct()
+        conectados = aristas.groupByKey().mapValues(list).flatMap(pista)
+        triciclos = conectados.groupByKey().mapValues(list).filter(condicion).flatMap(posibles_triciclos)
+        triciclos_locales = triciclos.collect() 
+        triciclos_locales_lst.extend(triciclos_locales)
+    print(triciclos_locales_lst)
+    
+    return triciclos_locales_lst
+
+if __name__ == "__main__":
+
+    if len(sys.argv) <= 2:
+        print(f"Usage: python3 {0} <filenames>")
+
+    else:
+        spc = SparkContext()
+        filenames = sys.arv[1:]
+        ejercicio3_grafos(spc, filenames)    
+
